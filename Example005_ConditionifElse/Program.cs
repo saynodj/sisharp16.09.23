ff9bde5b8a0d7dc0a22e35518417a312df0a856d
sergey <s.novak1990@mail.ru>
2023-09-23 21:09:57 +0300

    exampl 005 condition if-else

--- Example005_ConditionifElse/Program.cs
+++ Example005_ConditionifElse/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сережа") // ToLower помогает распознать имя с заглавной или с маленькой
+{
+    Console.WriteLine("Ура, это СЕРЕЖА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
