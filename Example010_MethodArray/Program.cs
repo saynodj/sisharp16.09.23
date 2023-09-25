702f9f88432346fa70aa4edf3156c4be26090acc
sergey <s.novak1990@mail.ru>
2023-09-26 01:02:53 +0300

    exampl 010 methodarray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// Имеется одномерный массив array из n элементов, требуется
+// найти элемент массива, равный find
+// 1. Установить счетчик index в позицию 0
+// 2. Если array [index] = find, алгоритм завершил работу
+// успешно.
+// 3. Увеличить index на 1
+// 4. Если index < n, то перейти к шагу 2. В противном случае
+// алгоритм завершил работу безуспешно.
+
+Console.Clear();
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
