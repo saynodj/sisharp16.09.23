b8dd79a51a7d3115495534f69f1de3ddaba67f99
sergey <s.novak1990@mail.ru>
2023-09-26 02:14:21 +0300

    exampl 011 arraylibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Если элемента нет, счеткик будет показывать *-1* то есть Такой элемент НЕ НАЙДЕН !
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4; // Принудительная 4 с индексом [4], что показать только первое вхождение
+array[6] = 4; // Принудительная 4 с индексом [6], что показать только первое вхождение
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444); 
+Console.WriteLine(pos);
